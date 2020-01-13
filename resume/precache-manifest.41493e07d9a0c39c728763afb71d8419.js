e4f31a403614df64f95f8416355ff799e96e7745
Robson Gian Perassoli <robsonperassoli@gmail.com>
2020-01-13 15:51:01 -0300

    new resume

---
+++ resume/precache-manifest.41493e07d9a0c39c728763afb71d8419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3472a33c4df5e6aeed5993c5c4e97a31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9d55e997bb645ede0cf",
+    "url": "/static/js/2.28feb396.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.28feb396.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f54137fee880f5a5c6a5",
+    "url": "/static/js/main.f07bfcf9.chunk.js"
+  },
+  {
+    "revision": "036061462ff286e65549",
+    "url": "/static/js/runtime-main.8abee554.js"
+  }
+]);
